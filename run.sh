39928bc9db5a469087bd34d4362b5ddb1f684bad
Harry Rong <harrykobe@gmail.com>
2018-05-02 14:32:44 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -9,18 +9,18 @@ FIXADS="-Duser.language=en -Duser.country=US"
 DEBUG_SSL=
 
 # Override etc/crowd.properties, e.g. from Docker
-if [ -n "${CROWD_APP_NAME+1}" ]; then
-  CROWD="-Dapplication.name=$CROWD_APP_NAME $CROWD" 
-fi
-if [ -n "${CROWD_APP_PW+1}" ]; then
-  CROWD="-Dapplication.password=$CROWD_APP_PW $CROWD" 
-fi
-if [ -n "${CROWD_APP_URL+1}" ]; then
-  CROWD="-Dapplication.login.url=$CROWD_APP_URL $CROWD" 
-fi
-if [ -n "${CROWD_URL+1}" ]; then
-  CROWD="-Dcrowd.server.url=$CROWD_URL $CROWD" 
-fi
+# if [ -n "${CROWD_APP_NAME+1}" ]; then
+#   CROWD="-Dapplication.name=$CROWD_APP_NAME $CROWD" 
+# fi
+# if [ -n "${CROWD_APP_PW+1}" ]; then
+#   CROWD="-Dapplication.password=$CROWD_APP_PW $CROWD" 
+# fi
+# if [ -n "${CROWD_APP_URL+1}" ]; then
+#   CROWD="-Dapplication.login.url=$CROWD_APP_URL $CROWD" 
+# fi
+# if [ -n "${CROWD_URL+1}" ]; then
+#   CROWD="-Dcrowd.server.url=$CROWD_URL $CROWD" 
+# fi
 
 
 # Run Server
